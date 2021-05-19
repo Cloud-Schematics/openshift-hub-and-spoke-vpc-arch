45bc6255e1559ba3dfab70b26357b8afbb14d0a9
jvallexm <jvallexm@gmail.com>
2021-05-19 11:45:12 -0400

    Fixed name issue

--- routing_table.tf
+++ routing_table.tf
@@ -12,7 +12,7 @@ resource ibm_is_vpc_routing_table_route hub_vpc_egress {
     vpc           = module.hub_vpc.vpc_id
     routing_table = ibm_is_vpc_routing_table.hub_vpc_routing_table.routing_table
     zone          = "${var.ibm_region}-${count.index + 1}"
-    name          = "${var.unique_id}-egress"
+    name          = "${var.unique_id}-egress-zone-${count.index + 1}"
     destination   = "0.0.0.0/0"
     action        = "delegate_vpc"
     next_hop      = ""
